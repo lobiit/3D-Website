cdb2bf5a9fb70965ad99134f57d4a898cdbd5ac9
Tumbo Lobi John <johnlobiit@gmail.com>
2023-04-17 18:11:47 +0300

    file upload functionality

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -43,6 +43,48 @@ const Customizer = () => {
         }
     }
 
+    const handleDecals = (type, result) => {
+        const decalType = DecalTypes[type];
+
+        state[decalType.stateProperty] = result;
+
+        if(!activeFilterTab[decalType.filterTab]) {
+            handleActiveFilterTab(decalType.filterTab)
+        }
+    }
+
+    const handleActiveFilterTab = (tabName) => {
+        switch (tabName) {
+            case "logoShirt":
+                state.isLogoTexture = !activeFilterTab[tabName];
+                break;
+            case "stylishShirt":
+                state.isFullTexture = !activeFilterTab[tabName];
+                break;
+            default:
+                state.isLogoTexture = true;
+                state.isFullTexture = false;
+                break;
+        }
+
+        // after setting the state, activeFilterTab is updated
+
+        setActiveFilterTab((prevState) => {
+            return {
+                ...prevState,
+                [tabName]: !prevState[tabName]
+            }
+        })
+    }
+
+    const readFile = (type) => {
+        reader(file)
+            .then((result) => {
+                handleDecals(type, result);
+                setActiveEditorTab("");
+            })
+    }
+
     return (
         <AnimatePresence>
             { !snap.intro && (

cdea0bd479787f28d73d38eb2305fe15b9611437
Tumbo Lobi John <johnlobiit@gmail.com>
2023-04-17 12:10:03 +0300

    added the go back button in the customizer page

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Tab = () => {
     return (
         <div>
-
+            Tab
         </div>
     );
 };
--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -5,18 +5,66 @@ import config from "../config/config.js";
 import state from "../store/index.js";
 import { download } from '../assets';
 import { EditorTabs, FilterTabs, DecalTypes} from "../config/constants.js";
+import {useSnapshot} from "valtio";
+import {fadeAnimation, slideAnimation} from "../config/motion.js";
+import {CustomButton, Tab} from "../components/index.js";
 
 const Customizer = () => {
+    const snap = useSnapshot(state)
     return (
-        <div>
-            <button className='download-btn' onClick={downloadCanvasToImage}>
-                <img
-                    src={download}
-                    alt='download_image'
-                    className='w-3/5 h-3/5 object-contain'
-                />
-            </button>
-        </div>
+        <AnimatePresence>
+            { !snap.intro && (
+                <>
+                    <motion.div
+                        key={"custom"}
+                        className={"absolute top-0 left-0 z-10"}
+                        {...slideAnimation('left')}
+                    >
+                        <div className={"flex items-center min-h-screen"}>
+                            <div className={"editortabs-container tabs"}>
+                                {EditorTabs.map((tab) => (
+                                    <Tab
+                                        key={tab.name}
+                                        tab={tab}
+                                        handleClick={()=> {}}
+                                    />
+                                ))}
+                            </div>
+                        </div>
+                    </motion.div>
+                    <motion.div className={"absolute z-10 top-5 right-5"}
+                                {...fadeAnimation}
+                    >
+                        <CustomButton
+                            type={"filled"}
+                            title={"Go Back"}
+                            handleClick={()=> state.intro=true}
+                            customStyles={"w-fit px-4 py-2.5 font-bold text-sm"}
+                        />
+                    </motion.div>
+                    <motion.div className={"filtertabs-container "} {...slideAnimation()}>
+                        {FilterTabs.map((tab) => (
+                            <Tab
+                                key={tab.name}
+                                tab={tab}
+                                isFilterTab
+                                isActiveTab={""}
+                                handleClick={()=> {}}
+                            />
+                            ))}
+                    </motion.div>
+                </>
+            )}
+        </AnimatePresence>
+        // <div>
+        //     <button className='download-btn' onClick={downloadCanvasToImage}>
+        //         <img
+        //             src={download}
+        //             alt='download_image'
+        //             className='w-3/5 h-3/5 object-contain'
+        //         />
+        //     </button>
+        // </div>
     );
 };
 

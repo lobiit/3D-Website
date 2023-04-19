aad3cff90be2663d7a8edb69b8368a3622b3912e
Tumbo Lobi John <johnlobiit@gmail.com>
2023-04-19 10:52:22 +0300

    created the ai picker component and added its functionality

--- src/components/AIPicker.jsx
+++ src/components/AIPicker.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import {CustomButton} from "./index.js";
 
-const AiPicker = () => {
+const AiPicker = ({prompt, setPrompt,generatingImg, handleSubmit }) => {
     return (
-        <div>
-            
+        <div className={"aipicker-container"}>
+
+            <textarea
+                rows={5}
+                value={prompt}
+                onChange={(e)=> setPrompt(e.target.value)}
+                placeholder={"Ask Ai"}
+                className={"aipicker-textarea"}
+            />
+            <div className={"flex flex-wrap gap-3"}>
+                {generatingImg ? (
+                    <CustomButton
+                        type={"outline"}
+                        title={"Asking AI..."}
+                        customStyles={"text-xs"}
+                    />
+                ):(
+                    <>
+
+                        <CustomButton
+                            type={"outline"}
+                            title={"Äi Logo"}
+                            handleClick={()=> handleSubmit('logo')}
+                            customStyles={"text-xs"}
+                        />
+                        <CustomButton
+                            type={"filled"}
+                            title={"AI Full"}
+                            handleClick={()=> handleSubmit('full')}
+                            customStyles={"text-xs"}
+                        />
+                    </>
+                )}
+            </div>
         </div>
     );
 };

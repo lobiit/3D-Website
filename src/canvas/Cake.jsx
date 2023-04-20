47f1d00584b7dcc1cb1027e478df2eb7c79084f5
Tumbo Lobi John <johnlobiit@gmail.com>
2023-04-20 11:31:03 +0300

    cake configuration and allowing resize

--- src/canvas/Cake.jsx
+++ src/canvas/Cake.jsx
@@ -1,50 +1,51 @@
-import React from 'react'
-import { easing } from 'maath';
-import { useSnapshot } from 'valtio';
+import React, {useMemo, useRef, useState} from 'react'
 import { useFrame } from '@react-three/fiber';
 import { Decal, useGLTF, useTexture } from '@react-three/drei';
-
 import state from '../store';
+import * as THREE from 'three';
+import {easing} from "maath";
+import {useSnapshot} from "valtio";
 
 const Cake = () => {
     const snap = useSnapshot(state);
-    const { nodes, materials } = useGLTF('/shirt_baked.glb');
-
+    const { nodes } = useGLTF('/cake_baked.glb');
     const logoTexture = useTexture(snap.logoDecal);
     const fullTexture = useTexture(snap.fullDecal);
 
-    useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
+    // Create a new material instance with the snap color
+    const material = useMemo(() => new THREE.MeshBasicMaterial({ color: snap.color }), [snap.color]);
+
+    // This reference will give us direct access to the mesh
+    const mesh = useRef();
+
+    // Set up state for the hovered and active state
+    const [hovered, setHover] = useState(false);
+    const [active, setActive] = useState(false);
+
+    // Subscribe this component to the render-loop, rotate the mesh every frame
+    useFrame((state, delta) => {
+        // Update the material color with easing
+        easing.dampC(material.color, snap.color, 0.25, delta);
+        // Swing the mesh based on mouse position if hovered
+        if (hovered) {
+            mesh.current.rotation.z = state.mouse.x * 0.5;
+        }
+    });
 
     const stateString = JSON.stringify(snap);
 
     return (
         <group key={stateString}>
-            <mesh
-                castShadow
-                geometry={nodes.T_Shirt_male.geometry}
-                material={materials.lambert1}
-                material-roughness={1}
-                dispose={null}
-            >
+            <mesh castShadow geometry={nodes.Frosted_Cake.geometry} material={material} dispose={null} scale={2.0}
+                  ref={mesh}
+                  onClick={(event) => setActive(!active)}
+                  onPointerOver={(event) => setHover(true)}
+                  onPointerOut={(event) => setHover(false)}>
                 {snap.isFullTexture && (
-                    <Decal
-                        position={[0, 0, 0]}
-                        rotation={[0, 0, 0]}
-                        scale={1}
-                        map={fullTexture}
-                    />
+                    <Decal position={[0, 0.1, 0]} rotation={[Math.PI / 2, 0, 0]} scale={0.8} map={fullTexture} />
                 )}
-
                 {snap.isLogoTexture && (
-                    <Decal
-                        position={[0, 0.04, 0.15]}
-                        rotation={[0, 0, 0]}
-                        scale={0.15}
-                        map={logoTexture}
-                        map-anisotropy={16}
-                        depthTest={false}
-                        depthWrite={true}
-                    />
+                    <Decal position={[0, 0.11, 0]} rotation={[Math.PI / 2, 0, 0]} scale={0.2} map={logoTexture} map-anisotropy={16} depthTest={false} depthWrite={true} />
                 )}
             </mesh>
         </group>
